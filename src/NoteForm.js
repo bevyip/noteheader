b6cf94aef4db158ad2ca048140f32f5e7bb8e2b9
hyip8000 <hyip@purdue.edu>
2017-06-14 16:41:37 -0400

    Creating methods to map list items

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,19 +2,56 @@ import React, { Component } from 'react';
 import './NoteForm.css'
 
 class NoteForm extends Component {
+  constructor(props){
+      super(props)
+      this.updateBody = this.updateBody.bind(this)
+      this.updateTitle = this.updateTitle.bind(this)
+      this.addEntry = this.addEntry.bind(this)
+      console.log(super.state)
+      this.state={
+        body: '',
+        title: '',
+      }
+  }
+
+  updateBody(ev){
+    ev.preventDefault()
+    this.setState({
+      body: ev.target.value
+    })
+  }
+
+  updateTitle(ev){
+    ev.preventDefault()
+    this.setState({
+      title: ev.target.value
+    })
+  }
+
+  addEntry(ev){
+    const state = {...this.state}
+    const entryLog ={
+      noteTitle: this.state.title,
+      noteBody: this.state.body,
+    }
+    super.state.entries.push(entryLog)
+    this.setState(state)
+  }
+
   render() {
     return (
       <div className="NoteForm">
           <form>
               <p>
-                <input type="text" name="title" placeholder="Title your note" value="" />
+                <input type="text" onChange={this.updateTitle} name="title" placeholder="Title your note" value="" />
               </p>
               <p>
-                <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                <textarea name="body" cols="30" rows="10" onChange={this.updateBody} placeholder="Just start typing..."></textarea>
               </p>
               <p>
                 <button 
                   className="submit"
+                  
                 ><span>
                   Submit
                   </span>
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,48 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './NoteList.css'
 
-const NoteList = () =>{
+class NoteList extends Component{
+  constructor(){
+    super()
+    this.state={
+      entries:[
+        {noteTitle: 'Citizens of distant epochs', noteBody: 'Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?'},
+        {noteTitle:'Preserve and cherish that pale blue dot', noteBody: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!"},
+        {noteTitle: 'Laws of physics', noteBody: ' Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas'},
+      ]
+    }
+  }
+
+  addEntry(item,index){
     return (
+            <li key={index}>
+              <div className="note">
+                <div className="note-title">
+                  {item.noteTitle}
+                </div>
+                <div className="note-body">
+                  <p>{item.noteBody}</p>
+                </div>
+              </div>
+            </li>
+    );
+}
+  render(){
+    return(
         <div className="NoteList">
             <h3>Notes</h3>
-        <ul id="notes">
-          <li>
-            <div className="note">
-              <div className="note-title">
-                Citizens of distant epochs
-              </div>
-              <div className="note-body">
-                <p>
-                  Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
-                </p>
-              </div>
-            </div>
-          </li>
-
-          <li>
-              <div className="note">
-                <div className="note-title">Preserve and cherish that pale blue dot </div>
-                <div className="note-body">
-                  <p>
-                    network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
-                  </p>
-                </div>
-              </div>
-            </li>
-
-            <li>
-              <div className="note">
-                <div className="note-title">Laws of physics</div>
-                <div className="note-body">
-                  <p>
-                    Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
-                  </p>
-                </div>
-              </div>
-            </li>
-        </ul>
+              <ul id="notes" >
+                {this.state.entries.map(this.addEntry)}
+            </ul>
          </div>
-    )
+    );
+  }
 }
 
 export default NoteList
