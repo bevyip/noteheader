e02c523d1b9675c72d9457b076388e8e2cbdbb5d
hyip8000 <hyip@purdue.edu>
2017-06-14 12:05:35 -0400

    End of class

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import quill from './quill.svg'
 import './SideBar.css'
 
 const Sidebar = () =>{
     return(
-        <p>SIDEBAR</p>
+       <div className="Sidebar">
+           <div className="logo">
+                <img src={quill} alt="Noteheader" />
+            </div>
+                <button className="new-note">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new-hover.png" alt="New note" />
+                <img className="outline" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new.png" alt="New note" />
+            </button>
+       </div>
     )
 }
 
